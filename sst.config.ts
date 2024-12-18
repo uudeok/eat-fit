c3c5fc7a13c07f5b25c8b11b795f9597cc3f7f17
서유덕 <awesome36@daum.net>
2024-12-18 22:49:16 +0900

    Release v0.2.4 : SST 배포 테스트 진행 IAM 권한 문제로 인한 재시도

--- sst.config.ts
+++ sst.config.ts
@@ -16,22 +16,3 @@ export default $config({
         });
     },
 });
-
-// /// <reference path="./.sst/platform/config.d.ts" />
-
-// export default $config({
-//     app(input) {
-//         return {
-//             name: 'eat-fit',
-//             removal: input?.stage === 'production' ? 'retain' : 'remove',
-//             home: 'aws',
-//         };
-//     },
-//     async run() {
-//         new sst.aws.Nextjs('eat-fit', {
-//             domain: {
-//                 name: 'eat-fit.net',
-//             },
-//         });
-//     },
-// });
