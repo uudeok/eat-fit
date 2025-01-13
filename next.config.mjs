c28fd87ec4a8be1d81137d63a00fd9cddbf0af7f
서유덕 <awesome36@daum.net>
2025-01-13 22:03:25 +0900

    Fix : 의존성 문제를 방지하기 위해 재설치 후 빌드

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    generateBuildId: 'eat-fit-build-id',
     reactStrictMode: false,
     poweredByHeader: false,
     webpack: (config) => {
