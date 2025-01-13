76a74a022f315bc72f7971a856435fc24f695d58
서유덕 <awesome36@daum.net>
2025-01-13 21:37:49 +0900

    Rename : BUILD_ID 고정 후 재배포 시도

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    generateBuildId: 'eat-fit-build-id',
     reactStrictMode: false,
     poweredByHeader: false,
     webpack: (config) => {
