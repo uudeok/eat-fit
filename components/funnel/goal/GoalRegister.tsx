4b0f670ec246ace644487f531f25ed0ef56a30fa
서유덕 <awesome36@daum.net>
2025-01-15 17:03:02 +0900

    buildspec.yml build_id copy 로직 변경 및 goal 생성 로직에 로그 삽입 후 배포시도

--- components/funnel/goal/GoalRegister.tsx
+++ components/funnel/goal/GoalRegister.tsx
@@ -12,6 +12,8 @@ const GoalRegister = () => {
     const { registerData } = useContext(FunnelContext);
     const { mutate: createGoal } = useCreateGoal();
 
+    console.log('register : ', registerData);
+
     const submitGoalData = () => {
         try {
             const createData = encodeCreateGoal({ ...registerData });
