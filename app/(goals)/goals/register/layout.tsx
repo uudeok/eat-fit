c2b73bb0811a31c26fa8a16694148aaa7036959d
서유덕 <awesome36@daum.net>
2025-01-17 13:20:50 +0900

    최상위 RootLayout 으로 provider 이동 후 테스트진행

--- app/(goals)/goals/register/layout.tsx
+++
@@ -1,8 +0,0 @@
-import FunnelProvider from '@/shared/context/FunnelProvider';
-import { ReactNode } from 'react';
-
-const GoalsLayout = ({ children }: { children: ReactNode }) => {
-    return <FunnelProvider>{children}</FunnelProvider>;
-};
-
-export default GoalsLayout;
