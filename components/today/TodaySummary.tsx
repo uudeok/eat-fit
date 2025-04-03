edc3b1258ca9ec8a93c8ea3a0b6ebfb3e56fe229
서유덕 <awesome36@daum.net>
2025-04-03 23:11:28 +0900

    Fix : build 에러 수정, /home 불필요한 로직 삭제

--- components/today/TodaySummary.tsx
+++ components/today/TodaySummary.tsx
@@ -3,17 +3,11 @@
 import NutrientSummary from './NutrientSummary';
 import TodayStatus from './TodayStatus';
 import { useFetchGoalsByStatus } from '@/service/queries';
-import { useRouter } from 'next/navigation';
 
 const TodaySummary = () => {
-    const router = useRouter();
     const { data: goalData } = useFetchGoalsByStatus('progress');
 
-    if (!goalData) {
-        alert('목표 설정이 되지 않았습니다');
-        router.push('/goals/register');
-        return;
-    }
+    if (!goalData) return;
 
     return (
         <div className='p-5 flex flex-col gap-4 bg-mainColor shadow-md"'>
